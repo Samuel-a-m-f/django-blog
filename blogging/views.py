cd528d05bcb09472b937933fe7d191f08cd98cfa
Samuel Foley <Samuel.a.foley@boeing.com>
2025-06-07 17:01:07 -0700

    Applied black

--- blogging/views.py
+++ blogging/views.py
@@ -21,9 +21,10 @@ def stub_view(request, *args, **kwargs):
 # and this view
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+    posts = published.order_by("-published_date")
+    context = {"posts": posts}
+    return render(request, "blogging/list.html", context)
+
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
@@ -31,5 +32,5 @@ def detail_view(request, post_id):
         post = published.get(pk=post_id)
     except Post.DoesNotExist:
         raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+    context = {"post": post}
+    return render(request, "blogging/detail.html", context)
